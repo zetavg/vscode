82bdc8f7a2ef79e4c2c64f300a5a90d6d68357c1
Pokai Chang <mail@zeta.vg>
2024-07-22 06:49:58 +0800

    [patch][ZP-197B] adjust status bar height

---
+++ src/vs/workbench/browser/parts/statusbar/consts.ts
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+// [ZP-197B] Adjust status bar height
+export const STATUS_BAR_HEIGHT = 28;
