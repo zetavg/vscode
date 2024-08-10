f4a717209290d0991c298e1d3cb40b6040118381
Pokai Chang <mail@zeta.vg>
2024-08-11 04:17:49 +0800

    [patch][ZP-D03B] globally accessible configuration service

--- src/vs/code/electron-utility/sharedProcess/sharedProcessMain.ts
+++ src/vs/code/electron-utility/sharedProcess/sharedProcessMain.ts
@@ -133,6 +133,9 @@ import { AllowedMcpServersService } from '../../../platform/mcp/common/allowedMc
 import { IMcpGalleryManifestService } from '../../../platform/mcp/common/mcpGalleryManifest.js';
 import { McpGalleryManifestIPCService } from '../../../platform/mcp/common/mcpGalleryManifestServiceIpc.js';
 
+// [ZP-D03B] Globally accessible configuration service.
+import { setConfigurationService } from 'vs/z-customizations/configurationService';
+
 class SharedProcessMain extends Disposable implements IClientConnectionFilter {
 
 	private readonly server = this._register(new UtilityProcessMessagePortServer(this));
@@ -268,6 +271,9 @@ class SharedProcessMain extends Disposable implements IClientConnectionFilter {
 		// Configuration
 		const configurationService = this._register(new ConfigurationService(userDataProfilesService.defaultProfile.settingsResource, fileService, policyService, logService));
 		services.set(IConfigurationService, configurationService);
+
+		// [ZP-D03B] Globally accessible configuration service.
+		setConfigurationService(configurationService);
 
 		// Storage (global access only)
 		const storageService = new RemoteStorageService(undefined, { defaultProfile: userDataProfilesService.defaultProfile, currentProfile: userDataProfilesService.defaultProfile }, mainProcessService, environmentService);
