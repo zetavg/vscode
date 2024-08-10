8c0ce7b493030d7e9e972c6e4c20daf8df3f5294
Pokai Chang <mail@zeta.vg>
2024-08-11 04:24:18 +0800

    [patch][ZP-F39A] customizable commit hash for REH

--- src/vs/workbench/electron-browser/desktop.main.ts
+++ src/vs/workbench/electron-browser/desktop.main.ts
@@ -68,6 +68,10 @@ import { MultiplexPolicyService } from '../services/policies/common/multiplexPol
 // [ZP-D03B] Globally accessible configuration service.
 import { setConfigurationService } from 'vs/z-customizations/configurationService';
 
+// [ZP-F39A] Customizable commit hash for REH.
+import { registerConfiguration as registerCustomizableREHCommitConfiguration } from 'vs/z-customizations/customizable-commit-for-reh/register-configuration';
+registerCustomizableREHCommitConfiguration();
+
 export class DesktopMain extends Disposable {
 
 	constructor(
