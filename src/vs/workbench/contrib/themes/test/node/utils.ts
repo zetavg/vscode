40a415fc6cbd79ca55226b82e8aadf76ffb065a5
zetavg <mail@zeta.vg>
2025-08-02 17:56:55 +0800

    [patch] theme color export utility

---
+++ src/vs/workbench/contrib/themes/test/node/utils.ts
@@ -0,0 +1,59 @@
+/* eslint-disable header/header */
+
+import { Color } from 'vs/base/common/color';
+import {
+	type ColorContribution,
+	type ColorDefaults,
+	ColorTransform,
+	ColorTransformType,
+	type ColorValue,
+} from 'vs/platform/theme/common/colorRegistry';
+
+export function isColorValueDerived(colorValue: ColorValue | null) {
+	if (!colorValue) {
+		return false;
+	}
+
+	if (colorValue instanceof Color) {
+		// Color object.
+		return false;
+	}
+
+	if (typeof colorValue === 'object') {
+		// ColorTransform
+		if (colorValue.op === ColorTransformType.OneOf) {
+			return colorValue.values.some(isColorValueDerived);
+		} else if (colorValue.op === ColorTransformType.IfDefinedThenElse) {
+			return isColorValueDerived(colorValue.then);
+		} else {
+			return isColorValueDerived(colorValue.value);
+		}
+	}
+
+	// string | ColorIdentifier
+	// If the string does not start with a hash (e.g.: #00aabb), we assume it's a reference to another color.
+	return !colorValue.startsWith('#');
+}
+
+export function isConsideredRequired(color: ColorContribution) {
+	const { defaults } = color;
+	if (!defaults) {
+		return true;
+	}
+
+	if (typeof defaults === 'object' && !(defaults instanceof Color) && !Reflect.has(defaults, 'op')) {
+		const d = defaults as ColorDefaults;
+		if (!d.dark && !d.light && d.hcDark && d.hcLight) {
+			// This might be a high contrast only color
+			return false;
+		}
+
+		return !isColorValueDerived(d.dark) || !isColorValueDerived(d.light);
+	}
+
+	return !isColorValueDerived(defaults as string | Color | ColorTransform);
+}
+
+export function defaultsToString(color: ColorContribution) {
+	return JSON.stringify(color.defaults);
+}
