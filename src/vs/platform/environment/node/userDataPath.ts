bb9bac68aa5887de2fdf85ed6f06ee5e68806eae
zetavg <mail@zeta.vg>
2025-08-03 11:21:21 +0800

    [patch] fixed app data path

--- src/vs/platform/environment/node/userDataPath.ts
+++ src/vs/platform/environment/node/userDataPath.ts
@@ -97,5 +97,9 @@ function doGetUserDataPath(cliArgs: NativeParsedArgs, productName: string): stri
 			throw new Error('Platform not supported');
 	}
 
+	if (productName !== 'code-oss-dev') {
+		productName = 'code-oss';
+	}
+
 	return join(appDataPath, productName);
 }
