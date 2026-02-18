b93cdafaceb54749b412484d5a488afaa3c7e246
zetavg <mail@zeta.vg>
2026-02-18 02:04:45 +0100

    [patch][ZP-43D0] hold-to-switch-model

--- src/vs/workbench/contrib/chat/common/constants.ts
+++ src/vs/workbench/contrib/chat/common/constants.ts
@@ -46,6 +46,9 @@ export enum ChatConfiguration {
 	ExplainChangesEnabled = 'chat.editing.explainChanges.enabled',
 	// [ZP-2C20] Show cost/usage in chat messages
 	ShowMoreResponseDetails = 'chat.showMoreResponseDetails',
+	// [ZP-43D0] Chat: hold-to-switch-mode
+	HoldCmdKeyAction = 'chat.editing.HoldCmdKeyAction',
+	HoldOptionKeyAction = 'chat.editing.holdOptionKeyAction',
 }
 
 /**
