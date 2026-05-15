bd919c1fa2729b28469a358146e5c1597a8181e7
zetavg <mail@zeta.vg>
2026-05-15 16:32:36 +0200

    [ZP-3528] Header height 28px.

--- src/vs/workbench/browser/part.ts
+++ src/vs/workbench/browser/part.ts
@@ -202,7 +202,8 @@ export abstract class Part<MementoType extends object = object> extends Componen
 
 class PartLayout {
 
-	private static readonly HEADER_HEIGHT = 35;
+	// [ZP-3528] Header height 28px.
+	// private static readonly HEADER_HEIGHT = 35;
 	private static readonly TITLE_HEIGHT = 35;
 	private static readonly Footer_HEIGHT = 35;
 
