8442318466565ea424d6a7eb26f1cdef72da0348
zetavg <mail@zeta.vg>
2026-05-15 16:21:10 +0200

    [patch][ZP-FID7] use Input font

--- src/vs/editor/common/config/fontInfo.ts
+++ src/vs/editor/common/config/fontInfo.ts
@@ -216,18 +216,32 @@ export const FONT_VARIATION_OFF = 'normal';
  */
 export const FONT_VARIATION_TRANSLATE = 'translate';
 
+
+/* [ZP-FID7] use Input font */
+// /**
+//  * @internal
+//  */
+// export const DEFAULT_WINDOWS_FONT_FAMILY = 'Consolas, \'Courier New\', monospace';
+// /**
+//  * @internal
+//  */
+// export const DEFAULT_MAC_FONT_FAMILY = 'Menlo, Monaco, \'Courier New\', monospace';
+// /**
+//  * @internal
+//  */
+// export const DEFAULT_LINUX_FONT_FAMILY = '\'Droid Sans Mono\', monospace';
 /**
  * @internal
  */
-export const DEFAULT_WINDOWS_FONT_FAMILY = 'Consolas, \'Courier New\', monospace';
+export const DEFAULT_WINDOWS_FONT_FAMILY = '\'InputMonoNarrow-ExtraLight\', Consolas, \'Courier New\', monospace';
 /**
  * @internal
  */
-export const DEFAULT_MAC_FONT_FAMILY = 'Menlo, Monaco, \'Courier New\', monospace';
+export const DEFAULT_MAC_FONT_FAMILY = '\'InputMonoNarrow-ExtraLight\', Menlo, Monaco, \'Courier New\', monospace';
 /**
  * @internal
  */
-export const DEFAULT_LINUX_FONT_FAMILY = '\'Droid Sans Mono\', monospace';
+export const DEFAULT_LINUX_FONT_FAMILY = '\'InputMonoNarrow-ExtraLight\', \'Droid Sans Mono\', \'monospace\', monospace';
 /**
  * @internal
  */
